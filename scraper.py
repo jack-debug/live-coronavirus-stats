d45462382e2e58d168d2180e07dd34cc4346970e
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-03-05 14:04:05 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
 from Tkinter import *
-import tkinter
-import tkmessagebox
+import Tkinter
 import requests
-import urllib.request
+import urllib
 import time
 import bs4
 import datetime
@@ -15,4 +14,7 @@ infected = soup.findAll('span')
 dead = soup.findAll('span')
 recovered = soup.findAll('span')
 
-print('Currently, the 2019 novel coronavirus on ' + datetime.datetime.now + ' has infected ' + infected + ' people and killed ' + dead + '. ' + recovered + ' have recovered.')
+top = Tkinter.Tk()
+text.insert(INSERT, "Currently, the 2019 novel coronavirus on " + datetime.datetime.now + " has infected " + infected + " people and killed " + dead + ". " + recovered)
+text.insert(END, " have recovered.")
+top.mainloop()
